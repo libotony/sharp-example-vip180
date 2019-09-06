7153398a08319d772c9a4959fe83fcdead699a0c
tony <tony.li@vechain.com>
2019-09-06 18:03:36 +0800

    add scripts

---
+++ scripts/check-balance.ts
@@ -0,0 +1,18 @@
+import 'sharp-cli'
+import { ContractMeta } from 'sharp'
+
+const account = '0x7567d83b7b8d80addcb281a71d54fc7b3364ffed'
+const contractAddress = '0x921b47242b347c5e214a26316e5c284fba8ebaff'
+
+const myTokenContract = require('../output/MyToken.json')
+const myToken = new ContractMeta(myTokenContract.abi, myTokenContract.bytecode)
+
+const connex = global.connex
+const balanceOf = connex.thor.account(contractAddress).method(myToken.ABI('balanceOf'))
+
+const main = async () => {
+    const ret = await balanceOf.call(account)
+    console.log(ret.decoded!['0'])
+}
+
+export default main
---
+++ scripts/deploy-my-token.ts
@@ -0,0 +1,29 @@
+import 'sharp-cli'
+import { ContractMeta, Awaiter } from 'sharp'
+
+const myTokenContract = require('../output/MyToken.json')
+const myToken = new ContractMeta(myTokenContract.abi, myTokenContract.bytecode)
+
+const thor = global.connex.thor
+const vendor = global.connex.vendor
+const wallet = global.wallet
+
+wallet.import('0xdce1443bd2ef0c2631adc1c67e5c93f13dc23a41c18b536effbbdcbcdb96fb65')
+
+const main = async () => {
+
+    const { txid } = await vendor
+        .sign('tx')
+        .request([myToken.deploy().asClause()])
+
+    console.log(`tx sent: ${txid}, waiting receipt......`)
+    const receipt = await Awaiter.receipt(thor.transaction(txid), thor.ticker())
+    if (receipt.reverted) {
+        console.log('Failed to deploy contract')
+    } else {
+        console.log('Contract deployed at ' + receipt.outputs[0].contractAddress)
+    }
+
+}
+
+export default main
